44a8e5fb8ef90999d99a3d261e34b963c76a4d96
Vasyl <vasylmiskiv@gmail.com>
2023-01-20 02:48:02 +0200

    projects query, projects, project card  ui were added

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import Header from "./components/Header.jsx";
 import Clients from "./components/Clients.jsx";
 import AddClientModal from "./components/AddClientModal.jsx";
+import Projects from "./components/Projects.jsx";
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -35,6 +36,7 @@ const App = () => {
       <Header />
         <div className="container">
           <AddClientModal />
+          <Projects />
           <Clients />
         </div>
       </ApolloProvider>
