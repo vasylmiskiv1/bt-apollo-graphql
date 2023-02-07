c0be238ea993211e5da5595a68e732eade66c41d
Vasyl Miskiv <vasylmiskiv@gmail.com>
2023-02-07 18:21:37 +0200

    set project to remote deployment

--- client/src/App.js
+++ client/src/App.js
@@ -26,7 +26,7 @@ const cache = new InMemoryCache({
 });
 
 const client = new ApolloClient({
-  uri: "http://localhost:5000/graphql",
+  uri: "http://192.168.0.102:5000/graphql",
   cache,
 });
 
--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const colors = require("colors");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 const cors = require("cors");
+const http = require("http");
+const path = require("path");
 require("dotenv").config();
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
@@ -13,11 +15,20 @@ connectDB();
 
 app.use(cors());
 
+{
+/* for SSR
+  const __rootdir = path.resolve();
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__rootdir, "client", "build", "index.html"));
+  });
+*/
+}
+
 app.use(
   "/graphql",
   graphqlHTTP({
     schema,
-    graphiql: process.env.NODE_ENV === "development",
+    graphql: process.env.NODE_ENV === "development",
   })
 );
 
